3c13c91a4a56db1b513a3cef45d3a6680dab38bb
Caique Sobral <51248056+CaiqueSobral@users.noreply.github.com>
2021-05-12 11:39:11 -0300

    added nested ifs and elifs to the code

---
+++ Code Day 3/Rollercoaster_elif.py
@@ -0,0 +1,14 @@
+print("Welcome to the Rollercoaster!")
+height = int(input("What is yout height in cm? "))
+
+if height >= 120:
+    print("You can ride the rollercoaster")
+    age = int(input("What is your age? "))
+    if age < 12:
+        print("Please pay R$5,00")
+    elif age <= 18:
+        print("Please pay R$7,00")
+    else:
+        print("Please pay R$12,00")
+else:
+    print("Sorry, you have to grow taller before you can ride")
