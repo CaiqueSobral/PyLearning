3a95c8eadeaba1fc40474e731b9c26c6874fba5d
Caique Sobral <51248056+CaiqueSobral@users.noreply.github.com>
2021-05-12 11:14:49 -0300

    added a folder for my days of training

--- Code/Sum2DigitNumber.py
+++
@@ -1,8 +0,0 @@
-two_digit_number = input("Type a two digit number: ")
-
-first_digit = int(two_digit_number[0])
-second_digit = int(two_digit_number[1])
-
-result = first_digit + second_digit
-
-print("A soma dos números é: " + str(result))
