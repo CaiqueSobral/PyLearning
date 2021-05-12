dd53a79432d6c43fb5bf5fd0c14feb93e12127e1
Caique Sobral <51248056+CaiqueSobral@users.noreply.github.com>
2021-05-12 12:32:48 -0300

    added a leap year checker

---
+++ Code Day 3/Leap_year_checker.py
@@ -0,0 +1,6 @@
+year = int(input("Wich year do you want to check? "))
+
+if year % 4 == 0 and year % 100 == 0 and year % 400 == 0:
+    print(f"{year} is a leap year")
+else:
+    print(f"{year} is not a leap year")
