f0a0b153c5826127ca890309d9b76845d00e8da6
Caique Sobral <51248056+CaiqueSobral@users.noreply.github.com>
2021-08-26 16:42:58 -0300

    add the starting code for day 8

---
+++ Code day 8 Functions with Inputs/1_Day_Start.py
@@ -0,0 +1,14 @@
+# def greet():
+#     print(f"Hello")
+#     print("How do you do")
+#     print("Isn't the weather nice today")
+# greet()
+
+def greet(name):
+    print(f"Hello {name}")
+    print(f"How do you do {name}?")
+
+nameUser = input("What is your name? ")
+greet(nameUser)
+
+
