e21496482eb808bdddaf779f108abc606b8dbc2f
Caique Sobral <51248056+CaiqueSobral@users.noreply.github.com>
2021-08-26 16:59:44 -0300

    imports the math module, for rouding numbers

--- Code day 8 Functions with Inputs/3_Paint_Area_Calculator.py
+++ Code day 8 Functions with Inputs/3_Paint_Area_Calculator.py
@@ -1,5 +1,7 @@
+import math
+
 def paintCalc(height, width, cover):
-    numberOfCans = round((height * width) / cover)
+    numberOfCans = math.ceil((height * width) / cover)
     print(f"You will need to use {numberOfCans} cans of paint.")
 
 paintCalc(3, 9, 5)
