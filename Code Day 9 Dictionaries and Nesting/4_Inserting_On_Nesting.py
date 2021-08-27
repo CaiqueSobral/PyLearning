033d95a5bbbff257e34639635e68c2f65613e9fb
Caique Sobral <51248056+CaiqueSobral@users.noreply.github.com>
2021-08-27 16:57:14 -0300

    add a start inserting code for nesting lists and dictionaries

---
+++ Code Day 9 Dictionaries and Nesting/4_Inserting_On_Nesting.py
@@ -0,0 +1,21 @@
+travel_log = [
+    {
+        "country": "France",
+        "visits": 12,
+        "cities": ["Paris", "Lille", "Dijon"]
+    },
+    {
+        "country": "Germany",
+        "visits": 5,
+        "cities": ["Berlin", "Hamburg", "Stuttgart"]
+    },
+]
+
+#TODO: Write the function that will allow new countries
+#to be added to the travel_log. 👇
+def addNewCountry():
+    print(travel_log)
+
+#🚨 Do not change the code below
+#addNewCountry("Russia", 2, ["Moscow", "Saint Petersburg"])
+print(travel_log[0])
