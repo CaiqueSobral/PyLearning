a7b87d494c6ecb1230688c215c94fc436e383daf
Caique Sobral <51248056+CaiqueSobral@users.noreply.github.com>
2021-08-27 17:18:49 -0300

    some commit

--- Code Day 9 Dictionaries and Nesting/4_Inserting_On_Nesting.py
+++ Code Day 9 Dictionaries and Nesting/4_Inserting_On_Nesting.py
@@ -14,7 +14,7 @@ travel_log = [
 #TODO: Write the function that will allow new countries
 #to be added to the travel_log. 👇
 def addNewCountry():
-    print(travel_log)
+    print("travel_log")
 
 #🚨 Do not change the code below
 #addNewCountry("Russia", 2, ["Moscow", "Saint Petersburg"])
