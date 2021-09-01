9e7da672d93034973d41f36d69444c0dee47cc91
Caique Sobral <51248056+CaiqueSobral@users.noreply.github.com>
2021-09-01 10:39:29 -0300

    add insert in nesting dictionary code

--- Code Day 9 Dictionaries and Nesting/4_Inserting_On_Nesting.py
+++ Code Day 9 Dictionaries and Nesting/4_Inserting_On_Nesting.py
@@ -13,9 +13,18 @@ travel_log = [
 
 #TODO: Write the function that will allow new countries
 #to be added to the travel_log. 👇
-def addNewCountry():
-    print("travel_log")
+def addNewCountry(country, visits, cities):
+    newCountry = {}
+    newCountry["country"] = country
+    newCountry["visits"] = visits
+    newCountry["cities"] = cities
 
+    travel_log.append(newCountry)
+    
 #🚨 Do not change the code below
 #addNewCountry("Russia", 2, ["Moscow", "Saint Petersburg"])
-print(travel_log[0])
+country = input("Qual país você visitou? \n")
+visits = int(input("Quantas vezes você visitou o país? \n"))
+cities = input("Quais cidades visitou? (Separe por virgula) \n").split(",")
+addNewCountry(country, visits, cities)
+print(travel_log)
