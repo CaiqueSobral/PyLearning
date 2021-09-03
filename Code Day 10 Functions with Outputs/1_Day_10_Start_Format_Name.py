01dc3812c6ef46beb2d43059ef2a8918a56897a2
Caique Sobral <51248056+CaiqueSobral@users.noreply.github.com>
2021-09-03 11:39:32 -0300

    rename de starting code for day 10

--- Code Day 10 Functions with Outputs/1_Day_10_Start_Format_Name.py
+++ Code Day 10 Functions with Outputs/1_Day_10_Start_Format_Name.py
@@ -1,10 +1,8 @@
-
 def FormatName(firstName, lastName):
+
     return (firstName + ' ' + lastName).title()
 
 fName = input("What is your first name? ")
 lName = input("What is your last name? ")
 
-fullName = FormatName(fName, lName)
-
-print(fullName)
+print(FormatName(fName, lName))
