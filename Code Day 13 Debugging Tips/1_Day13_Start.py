15a8c06605c614199810569b068d2dbe2f231e29
Caique Sobral <51248056+CaiqueSobral@users.noreply.github.com>
2021-09-09 14:37:05 -0300

    some exercises of debugging

--- Code Day 13 Debugging Tips/1_Day13_Start.py
+++ Code Day 13 Debugging Tips/1_Day13_Start.py
@@ -2,7 +2,7 @@
 
 # # Describe Problem
 # def my_function():
-#   for i in range(1, 20):
+#   for i in range(1, 21):
 #     if i == 20:
 #       print("You got it")
 # my_function()
@@ -10,15 +10,15 @@
 # # Reproduce the Bug
 # from random import randint
 # dice_imgs = ["❶", "❷", "❸", "❹", "❺", "❻"]
-# dice_num = randint(1, 6)
+# dice_num = randint(0, 5)
 # print(dice_imgs[dice_num])
 
-# # Play Computer
-# year = int(input("What's your year of birth?"))
-# if year > 1980 and year < 1994:
-#   print("You are a millenial.")
-# elif year > 1994:
-#   print("You are a Gen Z.")
+# Play Computer
+year = int(input("What's your year of birth?"))
+if year > 1980 and year < 1994:
+  print("You are a millenial.")
+elif year > 1994:
+  print("You are a Gen Z.")
 
 # # Fix the Errors
 # age = input("How old are you?")
